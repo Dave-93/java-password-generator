69e2483979aa381bec1e6d61a058f41a3313d267
Dave-93 <difrancesco.davide93@gmail.com>
2025-01-23 09:33:51 +0100

    feat: date part

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -8,11 +8,13 @@ public class PasswordGenerator {
         String name = scan.next();
         scan.nextLine();
         System.out.println("Inserisci il tuo cognome");
-        String surname = scan.nextLine();
+        String surname = scan.nextLine();//usato nextLine così mi permette di prendere correttamente un cognome formato da 2 parole
         System.out.println("Inserisci il tuo colore preferito");
         String color = scan.next();
-
-        
-        System.out.println("La tua password è " + name + "-" + surname.replace(" ", "") + "-" + color);
+        System.out.println("Inserisci la tua data di nascita");
+        String date = scan.next();
+        String [] dateSplit = date.split("/");
+        date = dateSplit[2];
+        System.out.println("La tua password è " + name + "-" + surname.replace(" ", "") + "-" + color + "-" + date);
     }
 }
